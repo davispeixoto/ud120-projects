f6438ff088d1ce0846cd04c4229dd302bbbaed76
Davis Peixoto <davis.peixoto@gmail.com>
2020-06-27 19:43:45 -0300

    regression lesson done
    Signed-off-by: Davis Peixoto <davis.peixoto@gmail.com>

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -17,21 +17,29 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load(open("../final_project/final_project_dataset_modified.pkl", "r"))
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
-target, features = targetFeatureSplit( data )
+# features_list = ["bonus", "long_term_incentive"]
+data = featureFormat(dictionary, features_list, remove_any_zeroes=True)
+target, features = targetFeatureSplit(data)
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
+print("Regressor slope" + str(reg.coef_))
+print("Regressor intercept" + str(reg.intercept_))
+
+print(reg.score(feature_test, target_test))
 
 ### Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
@@ -64,6 +72,8 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
