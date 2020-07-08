4ae0121c4f36d5a922c6c449d9b179b4c8df0391
Davis Peixoto <davis.peixoto@gmail.com>
2020-07-07 23:51:48 -0300

    finished evaluation metrics lesson
    Signed-off-by: Davis Peixoto <davis.peixoto@gmail.com>

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -27,5 +27,31 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+# print(accuracy_score([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], labels_test))
+
+# poi_count = len([poi for poi in pred if poi == 1.0])
+# print(poi_count)
+# print(len(features_test))
+
+for i in range(len(labels_test)):
+    if pred[i] == 1. and labels_test[i] == 1.:
+        print("Has a true positive at index %s") % str(i)
+
+# print(pred)
+# print(labels_test)
+
+print(accuracy_score(pred, labels_test))
+print(recall_score(pred, labels_test))
+print(precision_score(pred, labels_test))
+
+# predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+# true labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
